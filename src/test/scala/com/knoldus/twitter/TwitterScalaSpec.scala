df7fea694eec177513be7c2333ec266692332c5d
upanshu21 <upanshu21@gmail.com>
2020-03-06 00:28:27 +0530

    test cases added

--- src/test/scala/com/knoldus/twitter/TwitterScalaSpec.scala
+++ src/test/scala/com/knoldus/twitter/TwitterScalaSpec.scala
@@ -1,19 +1,54 @@
 package com.knoldus.twitter
 
-import org.scalatest.flatspec.AsyncFlatSpec
-import twitter4j.Status
 
-import scala.concurrent.Future
+import org.scalatest.flatspec.AsyncFlatSpec
+
+import twitter4j.{Query, _}
+
 
 class TwitterScalaSpec extends AsyncFlatSpec  {
 
-  var resultTweetList: Future[List[Status]]= _
+  var twitterObj: TwitterScala = _
 
-  var twitterScala : TwitterScala = _
+  val twitter = new TwitterScala
 
-  def beforeAll(): Unit = {
-    resultTweetList = Driver.resultTweetList
-    twitterScala = new TwitterScala
+  case class Status(createdAt: String, id :Int, id_Str : Int, text : String, user: String)
+  val list: List[Status] = List(Status("noon", 1, 2 , "messi is best" , "upanshu"))
+  val hashTags: String = "#fcb"
+  val query = new Query(hashTags)
+
+
+
+  "getTweets" should "retrieve tweets" in {
+
+    val actualResult = twitter.retrieveHashTagTweet(hashTags)
+
+    val actualBool = actualResult.map(x => x.nonEmpty)
+
+    val expected = true
+
+    actualBool.map(result => assert(result==expected))
   }
+
+  
+
+  "getLikesPerTweet" should "give number of average likes" in{
+
+
+    val actualResult = twitter.getLikesPerTweet(query)
+    val expectedCount = 0
+
+    actualResult.map(result => assert(result==expectedCount))
+  }
+
+  "getReTweetsPerTweet" should "give number of average re-tweets" in{
+
+    val actualResult = twitter.getReTweetsPerTweet(query)
+    val expectedCount = 0
+
+    actualResult.map(result => assert(result >= expectedCount))
+
+  }
+
 
 }
